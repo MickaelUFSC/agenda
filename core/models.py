e1cd753aa292077c82635fa598f79189183449e6
Mickael André da Silva <Mickael.a.s@outlook.com>
2023-07-20 16:34:42 -0300

    Criação de tela de edição, segurança para apenas o usuario poder editar os dados dele

--- core/models.py
+++ core/models.py
@@ -7,7 +7,9 @@ class Evento(models.Model):
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name="Data do evento")
     data_criacao = models.DateTimeField(auto_now=True)
+    local = models.CharField(max_length=200, blank=True, null=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
     class Meta:
         db_table = "evento"
 
@@ -16,3 +18,6 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y - %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
